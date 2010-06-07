69bc7aa31d3fc9e086659457b6f2ba46f7fc3fab
Pablo Seminario <pabluk@gmail.com>
2010-06-07 14:44:45 +0200

    Disutils files added.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'screenkey',
+    version = '0.1',
+    packages = ['Screenkey'],
+    package_dir = {'Screenkey': 'Screenkey'},
+    data_files = [('/usr/share/applications', ['data/screenkey.desktop'])],
+    scripts=['screenkey'],
+    author='Pablo Seminario',
+    author_email='pabluk@gmail.com',
+    platforms=['POSIX'],
+    license='GPLv3',
+    keywords='screencast keyboard keys',
+    url='http://launchpad.net/screenkey',
+    download_url='http://launchpad.net/screenkey/+download',
+    description='A screencast tool to display keys',
+    long_description="""
+    Screenkey is a useful tool for presentations or screencasts.
+    Inspired by ScreenFlick and based on the key-mon project code.
+    """,
+)
