1ae791bd581c062b6c364ec2c0d42a462e10fa5d
Yuri D'Elia <yuri.delia@eurac.edu>
2015-04-18 13:19:24 +0200

    Reduce the legalese clutter.

--- Screenkey/screenkey.py
+++ Screenkey/screenkey.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright (c) 2010 Pablo Seminario <pabluk@gmail.com>
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# "screenkey" is distributed under GNU GPLv3+, WITHOUT ANY WARRANTY.
+# Copyright(c) 2010-2012 Pablo Seminario <pabluk@gmail.com>
+# Copyright(c) 2015 by wave++ "Yuri D'Elia" <wavexx@thregr.org>.
 
 from __future__ import print_function, unicode_literals, division
 
